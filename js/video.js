af159cf38dc156bd9f836e4d5b27dd1895a0628e
AndrewShao628 <shaojinghong@gmail.com>
2024-04-07 22:58:43 -0400

    final?

--- js/video.js
+++ js/video.js
@@ -1,11 +1,61 @@
-var video;
+var video = document.getElementById("player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	document.getElementById("volume").innerHTML = ((100 * video.volume) + "%");
+	video.play();
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.90;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate / 0.90;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if(video.currentTime + 10 > video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime += 10;
+	}
+	console.log(video.currentTime);
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if(video.muted) {
+		video.muted = false;
+		document.getElementById("mute").innerHTML = "Mute";
+	}
+	else {
+		video.muted = true;
+		document.getElementById("mute").innerHTML = "Unmute";
+	}
+});
+
+document.querySelector("#slider").addEventListener("click", function() {
+	video.volume = this.value / 100;
+	document.getElementById("volume").innerHTML = (this.value + "%");
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
